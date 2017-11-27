d33499564d26d5d1858cc7b60209521395aaeeb2
wilsonah <wilsonah@rose-hulman.edu>
2017-11-27 16:30:17 -0500

    Completed all TODO's in m3

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -14,7 +14,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 """
 ########################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #  (Yes, that means for YOU to DO things per these instructions:)
 #
 # On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
 #   then selector  Mark Directory As --> Sources Root
@@ -74,10 +74,13 @@ matt.backward(50)
 matt.left(90)
 matt.forward(50)
 
+timmy = rg.SimpleTurtle('turtle')
+timmy.pen = rg.Pen('blue', 15)
+timmy.draw_circle(100)
 
 ########################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Add a few more line of your own code above to make one of the
 #   existing Turtles move some more and/or have different
 #   characteristics.
@@ -91,7 +94,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   The code above  CONSTRUCTS  two SimpleTurtle objects and gives those objects NAMES:
 #       dave    matt
 #
@@ -109,10 +112,16 @@ matt.forward(50)
 #   As always, test by running the module.
 #
 ########################################################################
+toytle = rg.SimpleTurtle('turtle')
+toytle.paint_bucket = rg.PaintBucket('purple')
+toytle.begin_fill()
+
+toytle.draw_regular_polygon(10, 20)
+toytle.end_fill()
 
 ########################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
