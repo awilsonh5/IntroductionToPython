d7b15ef07a0e0fc545d4bcb4dfe061aaa5523da5
wilsonah <wilsonah@rose-hulman.edu>
2017-11-27 17:06:51 -0500

    Was too ambitious for the checkerboard pattern

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,10 +2,10 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Andrew Wilson.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -28,3 +28,81 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+blue = rg.SimpleTurtle('turtle')
+blue.paint_bucket = rg.PaintBucket('blue')
+blue.speed = 40
+black = rg.SimpleTurtle('turtle')
+black.paint_bucket = rg.PaintBucket('black')
+black.speed = 40
+
+blue.forward(200)
+blue.left(90)
+blue.forward(200)
+blue.left(90)
+
+black.forward(200)
+black.left(90)
+black.forward(200)
+black.left(90)
+black.forward(10)
+
+for k in range(10):
+    blue.begin_fill()
+    black.begin_fill()
+    blue.draw_square(10)
+    blue.end_fill()
+    black.draw_square(10)
+    black.end_fill()
+    blue.forward(20)
+    black.forward(20)
+
+black.backward(10)
+blue.left(90)
+black.left(90)
+
+blue.forward(10)
+
+for k in range(10):
+    blue.begin_fill()
+    black.begin_fill()
+    blue.draw_square(10)
+    blue.end_fill()
+    black.draw_square(10)
+    black.end_fill()
+    blue.forward(20)
+    black.forward(20)
+
+blue.backward(10)
+blue.left(90)
+black.left(90)
+black.forward(10)
+
+for k in range(10):
+    blue.begin_fill()
+    black.begin_fill()
+    blue.draw_square(10)
+    blue.end_fill()
+    black.draw_square(10)
+    black.end_fill()
+    blue.forward(20)
+    black.forward(20)
+
+black.backward(10)
+blue.left(90)
+black.left(90)
+blue.forward(10)
+
+for k in range(10):
+    blue.begin_fill()
+    black.begin_fill()
+    blue.draw_square(10)
+    blue.end_fill()
+    black.draw_square(10)
+    black.end_fill()
+    blue.forward(20)
+    black.forward(20)
+window.close_on_mouse_click()
